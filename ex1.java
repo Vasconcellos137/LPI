315f9d15f505c811d0090455d8a2cda45ff4e298
Vasconcellos137 <vasconcellosvivih76@gmail.com>
2025-02-28 16:45:53 -0300

    Evolução

--- ex1.java
+++ ex1.java
@@ -1,3 +1,26 @@
+//01. Elaborar um algoritmo em fluxograma que leia a altura dos andares de
+//um prédio e o número de andares, calcule e imprima a altura do prédio.
+
+import java.util.Scanner;
+
 public class ex1 {
+    final static Scanner LER = new Scanner(System.in);
+    public static void main(String[] args) {
     
+        int altura = 0;
+        int nundares = 0;
+
+        System.out.println("Altura: ");
+        altura = LER.nextInt();
+
+        System.out.println("Número de Andares: ");
+        nundares = LER.nextInt();
+
+        int caulcuolo = 0;
+        caulcuolo = altura*nundares;
+
+
+        System.out.println("Resultado:" + caulcuolo);
+
+    }
 }
